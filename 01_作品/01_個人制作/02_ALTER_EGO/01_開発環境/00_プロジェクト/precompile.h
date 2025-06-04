1a150e55da4b79734b136169867e36badbc7cc23
Hieomu Otake <231057@st.yoshida-g.ac.jp>
2025-06-04 13:07:31 +0900

    ポートフォリオ追加

---
+++ 01_作品/01_個人制作/02_ALTER_EGO/01_開発環境/00_プロジェクト/precompile.h
@@ -0,0 +1,20 @@
+
+#include <Windows.h>
+#include <string>
+#include <time.h>
+#include <DirectXMath.h>
+#include <d3dx9.h>
+#include <random>
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <crtdbg.h>
+#include <unordered_map>
+#include <assert.h>
+#include <iomanip>
+#include "useful.h"
+
+#pragma once
---
+++ 01_作品/01_個人制作/02_ALTER_EGO/01_開発環境/00_プロジェクト/useful.h
@@ -0,0 +1,14 @@
+
+#include "main.h"
+
+#define RESET_INT   (0)
+#define RESET_FLOAT (0.0f)
+#define RESET_VEX3  (D3DXVECTOR3(0.0f,0.0f,0.0f))
+#define RESET_VEX2  (D3DXVECTOR2(0.0f,0.0f))
+#define RESET_WHITE (D3DXCOLOR(1.0f,1.0f,1.0f,1.0f))
+#define RESET_BLACK (D3DXCOLOR(0.0f,0.0f,0.0f,0.0f))
+#define RESET_RGB   (D3DXCOLOR(0.0f,1.0f,1.0f,1.0f))
+#define NUM_VTX     (4)
+#define HALF		(0.5f)
+
+#pragma once
